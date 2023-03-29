40c37cfa9d6fb7ddac40db6c5ec65d6c4a806aed
nikior9 <niki.or9@gmail.com>
2023-03-29 23:55:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const IMAGE_TABLE_FILE = 'image_table.txt';
 const IMAGE_SIZE = 150;
-const IMAGE_INTERVAL = 5000;
+const IMAGE_INTERVAL = 1000;
 const IMAGE_SPEED = 20;
 const imageTable = [];
 fetch(IMAGE_TABLE_FILE)
