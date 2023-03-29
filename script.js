315270156871ffd788980f6dc043dbc66fbaff3e
nikior9 <niki.or9@gmail.com>
2023-03-30 00:54:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
-const IMAGE_TABLE_FILE = 'image_table.txt'; // path to the text file containing image URLs and links
-const IMAGE_SIZE = 100; // width and height of images in pixels
-const IMAGE_INTERVAL = 1000; // time interval between image appearances in milliseconds
-const IMAGE_SPEED = 10; // speed at which images move down in pixels per second
-// load the image table from the text file
-const imageTable = [];
-fetch(IMAGE_TABLE_FILE)
-	.then(response => response.text())
-	.then(text => {
-		const lines = text.trim().split('\n');
-		lines.forEach(line => {
-			const [imageUrl, link] = line.split('::');
-			imageTable.push({ imageUrl, link });
-		});
-	});
-// create a new image element with a random position and link, and start its animation
-function createImage() {
-	// choose a random image URL and link from the table
-	const { imageUrl, link } = imageTable[Math.floor(Math.random() * imageTable.length)];
-	// create the image element and set its attributes
-	const img = document.createElement('img');
-	img.src = imageUrl;
-	img.width = IMAGE_SIZE;
-	img.height = IMAGE_SIZE;
-	img.style.left = `${Math.random() * (window.innerWidth - IMAGE_SIZE)}px`;
-	img.style.top = `${Math.random() * (window.innerHeighht - IMAGE_SIZE)}px`;
-	// add a click event listener to open the link in a new window
-	img.addEventListener('click', () => window.open(link));
-	// append the image element to the document body
-	document.body.appendChild(img);
-	// start the animation
-	let lastTime = null;
-	function animate(time) {
-		if (lastTime !== null) {
-			const deltaTime = (time - lastTime) / 1000;
-			img.style.top = `${parseFloat(img.style.top) + IMAGE_SPEED * deltaTime}px`;
-			if (parseFloat(img.style.top) > window.innerHeight) {
-				clearInterval(animationId);
-				document.body.removeChild(img);
-			} else if (parseFloat(img.style.top) % 10 === 0) {
-				const duplicate = img.cloneNode();
-				duplicate.style.opacity = 0.5;
-				document.body.appendChild(duplicate);
-			}
-		}
-		lastTime = time;
-	}
-	const animationId = setInterval(() => requestAnimationFrame(animate), 0);
+// Load the image/link pairs from a text file
+const imageLinks = [];
+fetch('image_table.txt')
+  .then(response => response.text())
+  .then(text => {
+    const lines = text.split('\n');
+    for (const line of lines) {
+      const [imageUrl, link] = line.split('::');
+      imageLinks.push({ imageUrl, link });
+    }
+  });
+
+// Create a function to generate a random position for an image
+function getRandomPosition() {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+  const x = Math.floor(Math.random() * (screenWidth - 100));
+  const y = Math.floor(Math.random() * (screenHeight - 100));
+  return { x, y };
 }
-// start creating images at the specified interval
-setInterval(createImage, IMAGE_INTERVAL);
+
+// Create a function to create a new image element with a given URL and link
+function createImageElement(imageUrl, link) {
+  const img = document.createElement('img');
+  img.src = imageUrl;
+  img.style.position = 'absolute';
+  const { x, y } = getRandomPosition();
+  img.style.left = `${x}px`;
+  img.style.top = `${y}px`;
+  img.addEventListener('click', () => window.open(link));
+  document.body.appendChild(img);
+  return img;
+}
+
+// Create a function to animate an image down the screen and leave duplicates behind
+function animateImage(img) {
+  const intervalId = setInterval(() => {
+    const { top } = img.getBoundingClientRect();
+    if (top + img.height >= window.innerHeight) {
+      clearInterval(intervalId);
+      img.remove();
+      return;
+    }
+    const duplicate = img.cloneNode();
+    document.body.appendChild(duplicate);
+    duplicate.addEventListener('click', () => window.open(img.link));
+    duplicate.style.top = `${top + 10}px`;
+  }, 100);
+
+  return intervalId;
+}
+
+// Start animating images with a 1 second interval
+let currentImageIndex = 0;
+setInterval(() => {
+  const { imageUrl, link } = imageLinks[currentImageIndex % imageLinks.length];
+  const img = createImageElement(imageUrl, link);
+  animateImage(img);
+  currentImageIndex++;
+}, 1000);
