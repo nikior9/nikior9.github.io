d8bfa9163a9c377e46a90b669d84b6ccb4c2a304
nikior9 <niki.or9@gmail.com>
2023-04-03 19:49:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const imgSize = 100; // in pixels
 const delay = 1000; // in milliseconds
 const speed = 100; // in pixels per second
 const duplicateTimeout = 2 * 60 * 1000; // in milliseconds
-
+//
 //document.body.style.cursor = *https://raw.githubusercontent.com/nikior9/nikior9.github.io/main/click.png*;
 
 fetch('images_table.txt')
